4687c0dd759c0c02f42c57624aa23063d952b9c6
Quentin Raynaud <qraynaud@users.noreply.github.com>
2015-09-06 19:31:35 +0200

    Auto install Package Control if necessary

--- install.sh
+++ install.sh
@@ -8,16 +8,17 @@ elif [[ "$unamestr" == 'FreeBSD' ]]; then
    platform='freebsd'
 fi
 
-if [[ $# == 0 ]]; then
-  $1='3'
+sublVersion="$1"
+if [ -z "$sublVersion" ]; then
+  sublVersion='3'
 fi
 
 if [[ $platform == 'linux' ]]; then
-  dir="$HOME/.config/sublime-text-$1"
+  dir="$HOME/.config/sublime-text-$sublVersion"
 elif [[ $platform == 'freebsd' ]]; then
-  dir="$HOME/.config/sublime-text-$1"
+  dir="$HOME/.config/sublime-text-$sublVersion"
 elif [[ $OSTYPE == darwin* ]]; then
-  dir="$HOME/Library/Application Support/Sublime Text $1"
+  dir="$HOME/Library/Application Support/Sublime Text $sublVersion"
 else
   echo 'Unknown OS'
   exit 1
@@ -26,10 +27,23 @@ fi
 mkdir -p $dir 2>/dev/null
 cd $dir
 
-if [ -r Packages ]; then
-  mv Packages Packages~
+if [ -d Packages ]; then
+  rm -Rf Packages.bck
+  mv Packages Packages.bck
 fi
 
 hg clone https://bitbucket.org/efolio/sublime-text-3-config Packages
+
+PACKAGE_CTRL_DEST='Installed Packages/Package Control.sublime-package'
+if [ ! -f "$PACKAGE_CTRL_DEST" ]; then
+  echo "Installing package control…"
+  mkdir "Installed Packages"
+  PACKAGE_CTRL_URL='http://packagecontrol.io/Package Control.sublime-package'
+  if hash curl 2>/dev/null; then
+    curl "$PACKAGE_CTRL_URL" > "$PACKAGE_CTRL_DEST"
+  else
+    wget "$PACKAGE_CTRL_URL" -O "$PACKAGE_CTRL_DEST"
+  fi
+fi
 
 echo '*** Install complete :)'
